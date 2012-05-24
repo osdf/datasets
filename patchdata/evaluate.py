Stupid bug in L2 distance! SIFT needs scale to be tested, not smoothing. Seems to produce paper's result. Scaling around 1.85 (logspace).

patchdata/evaluate.py
@@ -15,7 +15,7 @@ def cosine_dist(v1, v2):
 
 def l2_dist(v1, v2):
     """L2 distance between v1 and v2."""
-    return np.sqrt(np.sum(v1-v2)**2)
+    return np.sqrt(np.sum((v1-v2)**2))
 
 
 def ham_dist(v1, v2):
@@ -118,8 +118,10 @@ def evaluate(eval_set, distances, normalizations):
             m_dist = _histogram(matches, int(pairs), _dist_table[dist], _norm_table[norm])
             nonm_dist = _histogram(non_matches, int(pairs), _dist_table[dist], _norm_table[norm])
             curve = roc(m_dist, nonm_dist)
-            roc_pair[(dist, norm)] = {"fp_at_95": fp_at_95(curve), 
-                    "roc": curve, "m_dist": m_dist, "nonm_dist": nonm_dist}
+            fp95 = fp_at_95(curve)
+            print "Evaluate:", pairs, dist, norm, fp95
+            roc_pair[(dist, norm)] = {"fp_at_95": fp95, "roc": curve, 
+                    "m_dist": m_dist, "nonm_dist": nonm_dist}
         rocs[pairs] = roc_pair
     return rocs
 

patchdata/sift.py
@@ -8,57 +8,55 @@ from evaluate import evaluate
 from dataset import _default_path, patch_x, patch_y
 from vlfeat import vl_sift
 
+
 _pshape = (patch_x, patch_y)
 
-def store_as_sift(store, new_store, sigma, patch_shape=_pshape,
-        midx=31.5, midy=31.5, scale=5.3):
-    """Produce SIFT descriptors for patches in _store_ and
-    save in _new_store_. Smooth patches with sigma, compute
-    SIFT descriptor on reference frame [_midy_, _midx_, _scale_, 0].
-    """
-    frame = np.array([[midy, midx, scale, 0.]]).T
 
+def store_as_sift(store, new_store, patch_shape=_pshape,
+        midx=31.5, midy=31.5, scale=0.):
+    """Produce SIFT descriptors for patches in _store_ and
+    save in _new_store_. Compute
+    SIFT descriptor on reference frame [_midy_, _midx_, 2**_scale_, 0].
+    """
     for attrs in store.attrs:
         new_store.attrs[attrs] = store.attrs[attrs]
 
-    new_store.attrs["frame"] = frame
-    new_store.attrs["sigma"] = sigma
-    
     for key in store.keys():
         if type(store[key]) is h5py.Group:
             grp = new_store.create_group(name=key)
-            store_as_sift(store[key], grp, sigma, patch_shape, midx, midy, scale)
+            store_as_sift(store[key], grp, patch_shape, midx, midy, scale)
         if type(store[key]) is h5py.Dataset:
             dset = new_store.create_dataset(name=key, shape=(store[key].shape[0], 128), dtype=np.float64)
-            siftify(store[key], dset, sigma, patch_shape, frame)
+            frame = np.array([[midy, midx, scale, 0.]]).T
+            dset.attrs["frame"] = frame
+            siftify(store[key], dset, patch_shape, frame)
 
 
-def siftify(store, dataset, sigma, patch_shape, frame):
+def siftify(store, dataset, patch_shape, frame):
     """SIFT descriptor for every patch in _store_.
     
-    Smooth patches beforehand with gaussian of width _sigma_.
     Compute descriptor at frame _frame_.
     """
     totals = store.shape[0]
-    print "Generating ", totals, " SIFT descriptors for", store, "with sigma", sigma
+    print "Generating ", totals, " SIFT descriptors for", store, "at scale", frame[2]
     for i in xrange(totals):
-        patch = filters.gaussian_filter(store[i].reshape(patch_shape), sigma)
+        patch = store[i].reshape(patch_shape)
         [_, d] = vl_sift(patch.T, frames=frame)
         dataset[i] = np.asarray(d.ravel(), dtype=np.float64)
 
 
-def find_sigma(dataset, log_sigmas, path=_default_path):
-    """Find optimal sigma for SIFT descriptors.
+def find_scale(dataset, log_scales, path=_default_path):
+    """Find optimal scale for SIFT descriptors.
     """
     fname = "".join(["evaluate_", dataset, "_64x64.h5"])
     store = h5py.File(join(path, fname))
 
     rocs = dict()
-    dists = ["L2"]
-    norms = ["id", "l2", "l1"]
-    for sigma in log_sigmas:
-        sift = "".join(["evaluate_", dataset, "_sift_", str(sigma), ".h5"])
+    dists = ["L1", "L2", "COSINE"]
+    norms = ["l1", "l2", "id"]
+    for scale in log_scales:
+        sift = "".join(["evaluate_", dataset, "_sift_", str(scale), ".h5"])
         sift = h5py.File(join(path, sift), "w")
-        store_as_sift(store, sift, sigma=np.exp(sigma))
-        rocs[sigma] = evaluate(sift, distances=dists, normalizations=norms)
+        store_as_sift(store, sift, scale=2**scale)
+        rocs[scale] = evaluate(sift, distances=dists, normalizations=norms)
     return rocs