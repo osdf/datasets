165bbfca33636ffa64e91bffa8ed0c59f4b074af
osdf <osendorf@gmail.com>
2012-06-26 12:22:11 +0200

    A bit of cleaning up -- general helper module in root of repo. Supposed to handle processing, visualizing, ...

--- not_mnist/dataset.py
+++ not_mnist/dataset.py
@@ -6,16 +6,18 @@ http://yaroslavvb.blogspot.com/2011/09/notmnist-dataset.html
 
 
 import h5py
-import cPickle
 from os import listdir
 from os.path import dirname, join
-import glob
 
 import numpy as np
 try:
     import Image as img
 except:
     import PIL as img
+
+
+from ..helpers import shuffle_list
+
 
 _default_path = dirname(__file__)
 _default_name = join(_default_path, "not-mnist_28x28.h5")
@@ -78,68 +80,6 @@ def _create_grp(store, grp_name, path):
             tars[i] = int(ord(label) - ord('A'))
         except:
             pass
-            
-
-####
-#### should go in general helper module 
-####
-def shuffle_list(lst):
-    """Shuffle _lst_ 
-    """
-    n = len(lst)
-    for i in xrange(n):
-        interval = n - i
-        idx = i + np.random.randint(interval)
-        lst[i], lst[idx] = lst[idx], lst[i]
-
-
-def _scale_01(arr, eps=1e-10):
-    """Scale arr between [0,1].
-
-    Useful for gray images to be produced with PIL.
-    Does some contrast enhancement.
-    """
-    newarr = arr.copy()
-    mn = newarr.min()
-    newarr -= mn 
-    mx = newarr.max()
-    newarr *= 1.0/(mx + eps)
-    return newarr
-
-
-def visualize(array, rsz, xtiles=None, fill=0):
-    """Visualize flattened bitmaps.
-
-    _array_ is supposed to be a 1d array that
-    holds the bitmaps (of size _rsz_ each)
-    sequentially. _rsz_ must be a square number.
-
-    Specifiy the number of rows with _xtiles_.
-    If not specified, the layout is approximately
-    square. _fill_ defines the pixel border between
-    patches (default is black (==0)).
-    """
-    sz = array.size
-    fields = array.reshape(sz/rsz, rsz)
-    
-    # tiles per axes
-    xtiles = xtiles if xtiles else int(np.sqrt(sz/rsz))
-    ytiles = int(np.ceil(sz/rsz/(1.*xtiles)))
-    shape = int(np.sqrt(rsz))
-    
-    # take care of extra pixels for borders
-    pixelsy = ytiles * shape + ytiles + 1
-    pixelsx = xtiles * shape + xtiles + 1
-    # the tiling has this shape and _fill_ background
-    tiling = fill*np.ones((pixelsy, pixelsx), dtype = 'uint8')
-    
-    for row in xrange(ytiles):
-        for col in xrange(xtiles):
-            if (col+row*xtiles) < fields.shape[0]:
-                tile = fields[col + row * xtiles].reshape(shape, shape)
-                tile = _scale_01(tile) * 255
-                tiling[shape * row + row + 1:shape * (row+1) + row + 1, shape * col + col + 1:shape * (col+1) + col + 1] = tile
-    return img.fromarray(tiling)
 
 
 if __name__=="__main__":
