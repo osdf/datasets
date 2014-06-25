6da212502ab225fb4c5eb20716fb16c04b9f13e7
osdf <osendorf@gmail.com>
2014-06-25 20:59:33 +0200

    wip for pyramids. Tests missing.

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -8,7 +8,8 @@ import numpy as np
 import scipy.linalg as la
 import h5py
 from time import strftime
-
+import theano
+import theano.tensor as T
 
 try:
     import Image as img
@@ -663,6 +664,73 @@ def _stationary(store, key, new, pars):
     for attrs in store.attrs:
         new.attrs[attrs] = store.attrs[attrs]
     new.attrs['StationaryC'] = C
+
+
+def _gaussian2d(width, sigma):
+    """
+    Gaussian filter of total width _width_.
+    """
+    d, r = divmod(width, 2)
+    r = 1 if r==0 else 0
+    const = -2*sigma**2
+    grid = np.arange(-d + 0.5*r, d+1 - 0.5*r, 1)
+    grid = np.exp((grid**2)/const)
+    gridT = grid.reshape(grid.size, 1)
+    result = gridT*grid
+    return np.asarray(result/np.sum(result), dtype=theano.config.floatX)
+
+
+def _lcn_filters(fmaps, depth, width, sigma):
+    """
+    """
+    filters = np.zeros((fmaps, fmaps, width, width), dtype=theano.config.floatX)
+    d2 = depth//2
+    for i in xrange(fmaps):
+        for j in xrange(i-d2, i+d2+1):
+            if (j >= 0) and (j <fmaps):
+                filters[i, j, :, :] = gaussian2d(width, sigma)
+        fi_sum = np.sum(filters[i])
+        filters[i] /= fi_sum
+    return filters
+
+
+def _lcn(image, fmaps, pool_depth, width, sigma):
+    """
+    """
+    b, ch, r, c = image.shape
+    border = width//2
+    filters = lcn_filters(fmaps, pool_depth, width, sigma) 
+    filter_shape = filters.shape
+    blurred_mean = conv.conv2d(input=image, filters=filters, 
+            image_shape=image_shape, filter_shape=filter_shape,
+            border_mode='full')
+    image -= blurred_mean[:, :, border:-border, border:-border]
+    
+    image_sqr = T.sqr(image)
+    blurred_sqr = conv.conv2d(input=image_sqr, filters=filters, 
+            image_shape=image_shape, filter_shape=filter_shape,
+            border_mode='full')
+
+    div = T.sqrt(blurred_sqr[:, :, border:-border, border:-border])
+    fm_mean = div.mean(axis=[2, 3])
+    div = T.largest(fm_mean.dimshuffle(0, 1, 'x', 'x'), div) + 1e-6
+    image = image/div
+    return T.cast(image, theano.config.floatX)
+
+
+def _build_lcns(shape, depth, width, sigma):
+    """
+    Build _depth_ many local contrast normalizer
+    for usage in a pyramid.
+    """
+    lcns = []
+    for i in xrange(depth):
+        x = T.matrix('x')
+        x = x.reshape((1, 1, shape[0], shape[1]))
+        lcned = _lcn(x, fmaps=1, pool_detpth=1, width=width, sigma=sigma)
+        lcns.append(theano.function([x], lcned, allow_downcast=True))
+        shape = (shape[0]//2, shape[1]//2)
+    return lcns
 
 
 def _pyramid(store, key, new, pars):
