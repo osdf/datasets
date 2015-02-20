6d5d01e8d6daf7b6ecd8ddbfa813cbd16ef8b3d7
osdf <osendorf@gmail.com>
2015-02-20 12:04:42 +0100

    run ./download.sh to get the base data files.

---
+++ patchdata/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget http://www.cs.ubc.ca/~mbrown/patchdata/liberty.zip
+wget http://www.cs.ubc.ca/~mbrown/patchdata/notredame.zip
+wget http://www.cs.ubc.ca/~mbrown/patchdata/yosemite.zip
